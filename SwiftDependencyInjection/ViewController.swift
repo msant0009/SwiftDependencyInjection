76f42c46d2dfc7a7d74eedfb8ae366522cb41433
Mark Santoro <mark_santoro@comcast.net>
2024-09-09 19:49:20 -0700

    add a secondary view button

--- SwiftDependencyInjection/ViewController.swift
+++ SwiftDependencyInjection/ViewController.swift
@@ -11,9 +11,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemBlue
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 250, height: 250))
+        button.setTitle("Open another VC", for: .normal)
+        button.center = view.center
+        button.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
+        view.addSubview(button)
     }
 
+    
+    @objc private func buttonClicked() {
+        
+    }
 
 }
 
